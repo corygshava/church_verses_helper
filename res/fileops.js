3a3677200e9adfd32a25854adba6d5b0ab010df2
corygshava <corygshava@gmail.com>
2026-08-29 16:54:01 +0300

    removed all the stuff

--- res/fileops.js
+++
@@ -1,5 +0,0 @@
-// this is responsible for reading files and returning file lists and file contents
-
-import { readdir, stat } from "node:fs/promises";
-import path from "node:path";
-
--- startapp.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-color 0
-echo this script begins the Worship helper app and its remote, hit CTRL + C or close this window to end the app
-color 2
-echo starting server...
-start http://localhost:3000/
-node server.js
-pause
