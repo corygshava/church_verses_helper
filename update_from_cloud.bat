1b9a75c0832b527dfa3eac60b4f84eaf73dbb024
corygshava <corygshava@gmail.com>
2026-08-30 21:21:25 +0300

    updated the cloud updater to update everything including the boilerplate UI kit

--- update_from_cloud.bat
+++ update_from_cloud.bat
@@ -6,4 +6,13 @@ echo getting files from cloud source
 git pull
 
 echo applet updated
+
+echo updating the underlying ui kit
+cd public/cbl
+
+git reset --hard
+git pull
+
+echo UI kit updated successfully.
+
 pause
